777b701960208531309a2a8a662983fbe0e3857d
dungtt04 <tiendung2004lv@gmail.com>
2024-01-23 13:40:11 +0700

    Tuan3

---
+++ javascript/05/scripts.js
@@ -0,0 +1,11 @@
+// //map - sơ chế dữ liệu
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+// const newNumbers = numbers.map((item) => {
+//      return item * 2;
+// });
+// console.log(newNumbers);
+//filter: loại bỏ dữ liệu mình ko thích
+const newNumbers = numbers.filter((item) => {
+        console.log(item);k
+        return 
+    });
